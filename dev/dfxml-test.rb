98308e5320507e76a73d16fc3b95b90984d42da2
Donald Mennerich <donaldmennerich@nypl.org>
2012-07-30 12:05:05 -0400

    pronom

--- dev/dfxml-test.rb
+++ dev/dfxml-test.rb
@@ -2,17 +2,21 @@ require '../lib/dfxml'
 require '../lib/dfxml/parser'
 require 'nokogiri'
 
-reader = Nokogiri::XML::Reader(File.new('M1126-0008.xml'))
+reader = Nokogiri::XML::Reader(File.new('M1126-0022.xml'))
 
 docs = Array.new
 while reader.read
   if reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT and reader.name == 'fileobject'
-    puts "New File"
+
     file_object = Dfxml::Parser::FileObject.parse(reader.outer_xml)  
+    
+    puts file_object.filename
     if file_object.pronom_puid
-      puts file_object.pronom_format
-      puts file_object.pronom_puid
-      puts file_object.pronom_identification_method
+      puts "\t" << file_object.pronom_format
+      puts "\t" << file_object.pronom_puid
+      puts "\t" << file_object.pronom_identification_method
     end
+  elsif reader.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT and reader.name == 'ftype_str'
+    puts reader.read.value
   end
 end
